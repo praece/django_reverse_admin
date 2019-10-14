7c4ad481669d7b765362eac3f6d769111e3f1179
Kevin OBrien <kevin.obrien@praece.com>
2019-10-14 06:54:13 -0700

    Update __init__.py

--- django_reverse_admin/__init__.py
+++ django_reverse_admin/__init__.py
@@ -249,7 +249,7 @@ class ReverseModelAdmin(ModelAdmin):
                     if not isinstance(inline, ReverseInlineModelAdmin):
                         continue
                     obj_list = formset.save()
-                    if not getattr(new_object, inline.parent_fk_name):
+                    if not hasattr(new_object, inline.parent_fk_name):
                         obj = obj_list[0] if obj_list else None
                         setattr(new_object, inline.parent_fk_name, obj)
                 self.save_model(request, new_object, form, change=not add)
